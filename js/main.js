8c4cfb2a80324d5b56afbf084e1d0ca71c3c093b
Steve Gomez Boror <stevegomitas3@gmail.com>
2022-07-19 22:42:54 -0600

    Se creo la tabla de la coleccion de datos.

---
+++ js/main.js
@@ -0,0 +1,23 @@
+import {users} from './data.js'
+
+// CONECCION A LA ETIQUETA ROOT
+let root = document.getElementById("root");
+
+// VARIABLE QUE ALMACENA UN LISTADP ORDENADO HECHO CON HTML
+
+let table = '<table border="1">' + 
+                '<tr>' + '<td>' + 'Id' + '</td>' + '<td>' + 'User' + '</td>' + '<td>' + 'E-mail' + '</td>' + '</tr>' +
+                '<tr>' + '<td>' + users[0].id + '</td>' + '<td>' + users[0].username + '</td>' + '<td>' + users[0].email + '</td>' + '</tr>' +
+                '<tr>' + '<td>' + users[1].id + '</td>' + '<td>' + users[1].username + '</td>' + '<td>' + users[1].email + '</td>' + '</tr>' +
+                '<tr>' + '<td>' + users[2].id + '</td>' + '<td>' + users[2].username + '</td>' + '<td>' + users[2].email + '</td>' + '</tr>' +
+                '<tr>' + '<td>' + users[3].id + '</td>' + '<td>' + users[3].username + '</td>' + '<td>' + users[3].email + '</td>' + '</tr>' +
+                '<tr>' + '<td>' + users[4].id + '</td>' + '<td>' + users[4].username + '</td>' + '<td>' + users[4].email + '</td>' + '</tr>' +
+                '<tr>' + '<td>' + users[5].id + '</td>' + '<td>' + users[5].username + '</td>' + '<td>' + users[5].email + '</td>' + '</tr>' +
+                '<tr>' + '<td>' + users[6].id + '</td>' + '<td>' + users[6].username + '</td>' + '<td>' + users[6].email + '</td>' + '</tr>' +
+                '<tr>' + '<td>' + users[7].id + '</td>' + '<td>' + users[7].username + '</td>' + '<td>' + users[7].email + '</td>' + '</tr>' +
+                '<tr>' + '<td>' + users[8].id + '</td>' + '<td>' + users[8].username + '</td>' + '<td>' + users[8].email + '</td>' + '</tr>' +
+                '<tr>' + '<td>' + users[9].id + '</td>' + '<td>' + users[9].username + '</td>' + '<td>' + users[9].email + '</td>' + '</tr>' +
+             '</table>';
+
+// SE AGREGA LA ETIQUETA ROOT DEL DOM LA VARIABLE LISTADO
+root.innerHTML = table;
